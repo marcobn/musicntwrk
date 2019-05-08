381f8243efb3f54c8e44ea845eff4ca4a7538279
Marco Buongiorno Nardelli <mbn@unt.edu>
2019-05-08 13:31:04 +0200

    added sonification of spectrum with FIR using pyo - no csound needed

--- sonifiPy.py
+++ sonifiPy.py
@@ -20,8 +20,8 @@ import numpy.random as rn
 import sys,os,re
 import scipy.fftpack as FFT
 import random
-import matplotlib.pyplot as plt
 import music21 as m21
+import pyo as po
 
 # import CSound wrapper ctcsound (from csound installation directory)
 # path must be changed by user
@@ -257,7 +257,6 @@ def i_spectral2(xv,yv,itime,path='./',instr='noise'):
 	vel = vel/velmax
 	ftvel = FFT.irfft(vel)
 	ftvel = FFT.fftshift(ftvel)
-	print(ftvel[0])
 	f=open(path+'DSI_CSound.dat','w')
 	for line in range(int(nvel)):
 		f.write(str(ftvel[line])+'\n')
@@ -336,6 +335,89 @@ def i_spectral2(xv,yv,itime,path='./',instr='noise'):
 
 	# Clean up files
 	os.remove(path+'DSI_CSound.dat')
+	
+def i_specral_pyo(xv,yv,graphics=False):
+	# As i_spectral2 but uses pyo as audio engine
+	'''
+	# How to use the function 
+	# NOTE: there is an instability between pyo and matplotlib when using the 
+	# wx-based GUI - graphics MUST be set to False when running in a jupyter notebook
+	
+	import numpy as np
+	import sys,os,re,time
+
+	sys.path.append('/Users/marco/Dropbox (Personal)/Musica/Applications/musicntwrk')
+	from sonifiPy import *
+
+	path = './'
+	infile = 'DOSCAR.dat'
+	xv, y = r_1Ddata(path,infile)
+	s,a = i_specral_pyo(xv,y[0],graphics=True)
+	s.start()
+	time.sleep(5)
+	s.stop()
+	'''
+	nlines = xv.shape[0]
+
+	nbins = int(np.sqrt(nlines)-np.sqrt(nlines)%1)**2
+	while nbins > nlines or not(nbins != 0 and ((nbins & (nbins - 1)) == 0)):
+		nbins = int((np.sqrt(nbins)-1)**2)
+	yfft = np.zeros((nbins),dtype=int)
+	for n in range(nbins):
+		yfft[n] = n+1
+		
+	xminf = xv[0]
+	xmaxf = xv[-1]
+	xvf=np.asarray(xv)
+	xvs = (xv-xminf)/(xmaxf-xminf)*nbins
+	for line in range(nlines):
+		if xvs[line] >= nbins: xvs[line] = -1 
+		xvf[line] = yfft[int(xvs[line])]
+
+	# Normalization of the data shape into MIDI velocity
+
+	yminf = min(yv)
+	ymaxf = max(yv)
+	yvf=np.asarray(yv)
+	yvf = (yv-yminf)/(ymaxf-yminf)*127
+
+	vel=np.zeros((nbins),dtype=float)
+	nvel=0
+	for note in range(nbins):
+		for line in range(nlines):
+			if xvf[line] == yfft[note]:
+				vel[nvel] = yvf[line]
+				nvel=nvel+1
+				break
+
+	velmax = max(vel)
+	vel /= velmax
+	# FFT for FIR filter
+	ftvel = FFT.irfft(vel)
+	ftvel = FFT.fftshift(ftvel)
+
+	# start the pyo server 
+	s = po.Server().boot()
+
+	# signal to filter 
+	sf = po.PinkNoise(.5)
+
+	# FIR filter
+	# Create a table of length `buffer size`
+	bs = ftvel.shape[0]
+	# Create a table of length `buffer size`
+	t = po.DataTable(size=bs)
+	osc = po.TableRead(t)
+	# Share the table's memory with a numpy array.
+	arr = np.asarray(t.getBuffer())
+	# assign ftvel to the table memory buffer
+	arr[:] = ftvel
+
+	# do the convolution
+	a = po.Convolve(sf, table=t, size=t.getSize(), mul=.5).mix(2).out()
+
+	if graphics: s.gui(locals())
+	return(s,a)
 
 def i_time_series(xv,yv,path='./',instr='csb701'):
 	
