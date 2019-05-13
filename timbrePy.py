f33ac8cc446d855cac137968a8114615de7441ff
Marco Buongiorno Nardelli <mbn@unt.edu>
2019-05-13 11:11:12 +0200

    added standardized MF power spectrum

--- timbrePy.py
+++ timbrePy.py
@@ -284,6 +284,38 @@ def computeStandardizedMFCC(input_path,input_file,nmel=16,nmfcc=13,lmax=None,max
 	mfcc = np.asarray(mfcc)
 	return(np.sort(waves),mfcc,lmax)
 
+def computeStandardizedMFPS(input_path,input_file,nmel=16,lmax=None,maxi=None,nbins=None):
+	# read audio files in repository and compute the standardized (equal number of samples per file) 
+	# and normalized MFCC
+	waves = list(glob.glob(os.path.join(input_path,input_file)))
+	wf = []
+	for wav in np.sort(waves):
+		y, sr = librosa.load(wav)
+		wf.append(y)
+	wf = np.asarray(wf)
+	# standardization of the number of sample in every sound wav
+	if lmax == None:
+		lwf = []
+		for n in range(wf.shape[0]):
+			lwf.append(wf[n].shape[0])
+		lwf = np.asarray(lwf)
+		lmax = np.max(lwf)
+	mfcc = []
+	for n in range(wf.shape[0]):
+		if wf[n].shape[0] <= lmax:
+			wtmp = np.pad(wf[n], (0, lmax-wf[n].shape[0]), 'constant')
+		else:
+			wtmp = wf[n][:lmax]
+		if nbins == None:
+			hopl = 512
+		else:
+			hopl = hopl = int((lmax/nbins)*2/2+1) #round(int(lmax/nbins)/2)*2
+		S = librosa.feature.melspectrogram(wtmp, sr=sr, n_mels=nmel,hop_length=hopl)
+		log_S = librosa.power_to_db(S, ref=np.max)
+		mfcc.append(log_S)
+	mfcc = np.asarray(mfcc)
+	return(np.sort(waves),mfcc,lmax)
+
 def computeStandardizedPSCC(input_path,input_file,npscc=13,lmax=None,maxi=None,nbins=None):
 	# read audio files in repository and compute the standardized (equal number of samples per file) 
 	# and normalized PSCC
