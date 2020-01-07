8875edfd7d9e6d6336a022bed7d66ad31cc9a5fc
Marco Buongiorno Nardelli <mbn@unt.edu>
2020-01-07 15:33:42 -0600

    addition to scaleFreeFit

--- pcsPy.py
+++ pcsPy.py
@@ -1265,7 +1265,7 @@ def plotOpsHistogram(newvalues,newcounts,fx=15,fy=4):
 #     plt.xticks([])
     plt.bar(newvalues,newcounts,width=0.85,color='grey')
 
-def scaleFreeFit(Gx,plot=True):
+def scaleFreeFit(Gx,indeg=True,imin=0,undir=False,lfit='powerlaw',plot=True):
     # Fits the degree distribution to a power law - check for scale free network
     def curve_fit_log(xdata, ydata) :
         xdata = np.array(xdata)
@@ -1276,26 +1276,45 @@ def scaleFreeFit(Gx,plot=True):
     #   Fit data to a power law in loglog scale (linear)
         xdata_log = np.log10(xdata)
         ydata_log = np.log10(ydata)
-        linlaw = lambda x,a,b: a+x*b
+        if lfit == 'powerlaw':
+            linlaw = lambda x,a,b,L: a+x*b
+        elif lfit == 'truncatedpowerlaw':
+            linlaw = lambda x,a,b,L: a+x*b+L*np.exp(x)
+        else:
+            print('lfit not defined')
         popt_log, pcov_log = curve_fit(linlaw, xdata_log, ydata_log)
         ydatafit_log = np.power(10, linlaw(xdata_log, *popt_log))
         return (xdata,ydata,popt_log, pcov_log, ydatafit_log)
-    degree_sequenceJ = ((sorted([d for n, d in Gx.in_degree()],reverse=True)))
-    degreeCount = collections.Counter(degree_sequenceJ)
+    try:
+        if indeg == True:
+            data = np.array(sorted([d for n, d in Gx.in_degree()],reverse=True))
+        elif indeg == False and undir == False:
+            data = np.array(sorted([d for n, d in Gx.out_degree()],reverse=True))
+        elif indeg == False and undir == True:
+            data = np.array(sorted([d for n, d in Gx.degree()],reverse=True))
+    except:
+        data = Gx
+    data = data[imin:]
+    degreeCount = collections.Counter(data)
     deg, cnt = zip(*degreeCount.items())
 
     deg,cnt,popt,_,fit = curve_fit_log(deg,cnt)
     
     if plot:
         plt.loglog(deg,cnt, 'bo')
+#         fit = 10**popt[0]*np.power(deg,popt[1])
         plt.loglog(deg,fit, 'r-')
-        # fit = 10**popt[0]*np.power(deg,popt[1])
         plt.ylabel("Count")
         plt.xlabel("Degree")
         plt.show()
-    print('power low distribution - count = ',10**popt[0],'*degree^(',popt[1],')')
+    if lfit == 'powerlaw':
+        print('power low distribution - count = ',10**popt[0],'*degree^(',popt[1])
+    elif lfit == 'truncatedpowerlaw':
+        print('power low distribution - count = ',10**popt[0],'*degree^(',popt[1],'* e^',popt[2])
+    else:
+        print('lfit not defined')
     return(deg,cnt,fit)
-
+    
 def powerFit(Gx,mode='power_law',xmin=None,xmax=None,linear=False,indeg=True,undir=False,units=None):
     # set-up
     import pylab
