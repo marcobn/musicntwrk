39b1d0c39eb1d0140ce45bac5bc1aa765a38165e
Marco Buongiorno Nardelli <mbn@unt.edu>
2020-02-06 11:39:35 -0600

    setup as python package

--- SRC/pcsPy.py
+++ SRC/pcsPy.py
@@ -24,6 +24,7 @@ import networkx as nx
 import community as cm
 import music21 as m21
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 import vpython as vp
 
 from scipy.optimize import curve_fit
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='musicntwrk',
+	version='1.0',
+	description='music as data, data as music',
+	author='Marco Buongiorno Nardelli',
+	author_email='mbn@unt.edu',
+	platforms='Linux',
+	url='www.musicntwrk.com',
+	package_dir={'': 'SRC'},
+	packages=['']
+	)
