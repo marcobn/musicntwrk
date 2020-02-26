f3c3c347ada8c7012d77e293d45500ff65bbd473
Marco Buongiorno Nardelli <mbn@unt.edu>
2020-02-26 12:59:54 -0600

    objectified versions

--- TONALHARMONY/tonalHarmonyDefs.py
+++ TONALHARMONY/tonalHarmonyDefs.py
@@ -15,7 +15,7 @@
 #
 
 from pcsPy import *
-import pickle
+import pickle, copy
 import PySimpleGUI as sg
 
 def shortHands():
@@ -176,7 +176,178 @@ def applyOps(name,chord):
                                 print(PCSet(selfto).normalOrder().tolist())
                 else:
                         print('operation not defined')
-                        
+
+def tonalPartition(nodes,Gx,Gxu,resolution=1.0,display=False):
+    part = cm.best_partition(Gxu,resolution=resolution)
+    dn = np.array(nodes)
+    labe = []
+    modu = []
+    modul = []
+    for m in range(len(dn)):
+        labe.append(str(dn[m][0]))
+        modu.append(part[str(m)])
+        modul.append([str(dn[m][0]),Gx.degree()[str(m)],part[str(m)]])
+    modul = pd.DataFrame(modul,columns=['Label','Degree','Modularity'])
+    moduldict = dict(zip(labe,modu))
+
+    if display:
+    # display the score with modularity classes
+        mc = []
+        seqmc = []
+        for n in range(len(seq)):
+            p = PCSet(np.asarray(seq[n]))
+            nn = ''.join(m21.chord.Chord(p.normalOrder().tolist()).pitchNames)
+            mc.append(nn)
+            seqmc.append(moduldict[nn])
+
+        l = 0
+        analyzed = copy.deepcopy(chords)
+        for c in analyzed.recurse().getElementsByClass('Chord'):
+            c.closedPosition(forceOctave=4,inPlace=True)
+            c.addLyric(str(l))
+            c.addLyric(seqmc[l])
+            l += 1
+        analyzed.show('musicxml')
+    return(moduldict,modul)
+
+def scoreAnalysis(seq,moduldict,keydict,keychange=None,altrn=None,table='',verbose=False):
+# Score analysis
+
+# Read tonal harmony model
+    f = open(table,'rb')
+    head = pickle.load(f)
+    table = pickle.load(f)
+    f.close()
+    tab = np.array(table)
+    
+# Dictionary of enharmonics for notes in music21 Chords
+    enharmonicDict = enharmonicDictionary()
+# Dictionary of shorthand symbols for rn extensions
+    figureShorthands = shortHands()
+
+# Determination of operators
+    try:
+        ntot = seq.shape[0]
+    except:
+        ntot = len(seq)
+    ops = []
+    for i in range(ntot):
+        if i < ntot-1: 
+            ops.append(generalizedOpsName(seq[i],seq[i+1])[1])
+
+# First chord
+    rn = []
+    ch = np.copy(seq[0])
+    for n in range(1,len(ch)):
+        if ch[n] < ch[n-1]: ch[n] += 12
+    ch += 60
+    n = m21.chord.Chord(ch.tolist())
+    chord = ''.join(n.pitchNames)
+    key = keydict[moduldict[chord]] 
+    rn.append(m21.roman.romanNumeralFromChord(n, m21.key.Key(key)).figure)
+    # Full score
+    nxt = ntot-1
+    i = 0
+    check = 0
+    while i < nxt:   
+        try:
+    #         Manual control of rn (when needed)
+            if altrn != None and i in altrn.keys():
+                rn.append(altrn[i])
+            else:
+                idx,idy = np.where(tab == ops[i])
+                tmp = []
+                for n in range(len(idy)):
+                    if (rn[i] == str(head[idx[n]])):
+                        tmp.append(head[idy[n]])
+                if len(tmp) == 1:
+                    rn.append(tmp[0])
+                else:
+                    chord = ''.join(m21.chord.Chord(PCSet(seq[i]).normalOrder().tolist()).pitchNames)
+                    key = keydict[moduldict[chord]]
+                    for n in range(len(tmp)):
+                        ch = m21.roman.RomanNumeral(tmp[n],m21.key.Key(key)).pitchClasses
+                        if PCSet(ch).normalOrder().tolist() == seq[i+1]:
+                            rn.append(str(tmp[n]))
+                            break
+            i += 1
+        except Exception as e:
+            if verbose: print(i,'try',type(e),e,chord)
+            try:
+                if check == i:
+                    print('check error')
+                    break
+                else:
+                    print('modulation at or before chord no. ',i)
+                    check = i
+                    rn.pop()
+                    ch = np.copy(PCSet(seq[i-1]).normalOrder().tolist())
+                    for n in range(1,len(ch)):
+                        if ch[n] < ch[n-1]: ch[n] += 12
+                    ch += 60
+                    m = m21.chord.Chord(ch.tolist())
+                    key = keydict[moduldict[''.join(m.pitchNames)]]
+#                     Manual control of modulations (when needed)
+                    if keychange != None and i in keychange.keys():
+                        key = keychange[i]
+                    p = []
+                    for c in ch:
+                        p.append(enharmonicDict[key][c])
+                    n = m21.chord.Chord(p)
+                    chord = ''.join(n.pitchNames)                
+                    try:
+                        rnum = m21.roman.romanNumeralFromChord(n,m21.key.Key(key)).romanNumeralAlone
+                        fig = m21.roman.postFigureFromChordAndKey(n,m21.key.Key(key))
+                        try:
+                            fig = figureShorthands[fig]
+                        except:
+                            pass
+                        rn.append(rnum+fig)
+                        if verbose: print(i-1,'except',n,rn[i-1],key,'\n')
+                        i -= 1
+                    except Exception as e:
+                        print(type(e),e,chord)
+                        break
+            except Exception as e:
+                print(type(e),e) 
+                break
+    nxt = i
+    return(nxt,rn,ops)
+
+def showAnalysis(nt,chords,seq,rn,ops,keydict,moduldict,last=False,display=False):
+# Create dictionary of score analysis
+    reference = []
+    for n in range(nt):
+        try:
+            chord = ''.join(m21.chord.Chord(PCSet(seq[n]).normalOrder().tolist()).pitchNames)
+            entry = [PCSet(seq[n]).normalOrder(),chord,rn[n],ops[n],keydict[moduldict[chord]],moduldict[chord]]
+            reference.append(entry)
+        except:
+            pass
+    if last:
+    # Add last chord
+        ops.append(' ')
+        chord = ''.join(m21.chord.Chord(PCSet(seq[nt]).normalOrder().tolist()).pitchNames)
+        entry = [PCSet(seq[nt]).normalOrder(),chord,rn[nt],ops[nt],keydict[moduldict[chord]]]
+        reference.append(entry)
+
+    # Set dictionary as pandas dataframe
+    analysis = pd.DataFrame(reference,columns=['pcs','chord','rn','ops','key','modul'])
+    
+    if display:
+        # display the analyzed score
+        l = 0
+        analyzed = copy.deepcopy(chords)
+        for c in analyzed.recurse().getElementsByClass('Chord'):
+            c.closedPosition(forceOctave=4,inPlace=True)
+            c.addLyric('')
+            c.addLyric('')
+        #     c.addLyric(str(ops[l]))
+            c.addLyric(str(rn[l]))
+            l += 1
+        analyzed.show('musicxml')
+    return(analysis)
+
 def clearOutput():
         return
 
