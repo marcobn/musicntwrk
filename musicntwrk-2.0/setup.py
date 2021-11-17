2c19dc0056b2b0b2b3332d7af6ad1be9a982742c
Marco Buongiorno Nardelli <mbn@unt.edu>
2021-11-17 10:34:24 -0600

    2.2.7

--- musicntwrk-2.0/setup.py
+++ musicntwrk-2.0/setup.py
@@ -23,7 +23,7 @@ with open(path.join(this_directory, 'README.md')) as f:
 
 
 setup(name='musicntwrk',
-	version='2.2.6',
+	version='2.2.7',
 	description='music as data, data as music',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
