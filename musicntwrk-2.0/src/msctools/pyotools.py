c015f5c2a611b22b9590529ec6ad7e61e7a93de6
Marco Buongiorno Nardelli <mbn@unt.edu>
2024-09-19 17:32:00 -0500

    updated optimizer for keras models

--- musicntwrk-2.0/src/msctools/pyotools.py
+++ musicntwrk-2.0/src/msctools/pyotools.py
@@ -338,6 +338,71 @@ def importClips(dirpath='./',filepath='./'):
         print('error in file reading')
         return
 
+# helper class to play a single clip
+class pyoClip(pyoObject):
+    
+    # helper class to play a clip
+
+    def __init__(self, input, speed=1, loop=False, offset=0, interp=2, mul=1, add=0):
+        
+        PyoObject.__init__(self)
+
+        # Keep references of all raw arguments
+        self.input = input
+        self.speed = speed
+        self.loop = loop
+        self.offset = offset
+        self.interp = interp
+        self.mul = mul
+        self.add = add
+
+        # Using InputFader to manage input sound allows cross-fade when changing sources
+        self.in_fader = pyo.InputFader(input)
+
+        # Convert all arguments to lists for "multi-channel expansion"
+        in_fader, speed, loop, offset, interp, mul, add = pyo.convertArgsToLists(self._in_fader, speed, loop, offset, interp, mul, add)
+
+    # Set methods and attributes
+    def setInput(self, x, fadetime=0.05):
+        """
+        Replace the `input` attribute.
+
+        :Args:
+
+            x : PyoObject
+                New signal to process.
+            fadetime : float, optional
+                Crossfade time between old and new input. Defaults to 0.05.
+
+        """
+        self.input = x
+        self.in_fader.setInput(x, fadetime)
+
+    def setSpeed(self, x):
+        # set speed
+        self.speed = x
+
+    def setLoop(self,x):
+        # set loop (True/False)
+        self.loop = x
+
+    def setOffset(self, x):
+        # set offset
+        self.offset = x
+
+    def setInterp(self, x):
+        # set offset
+        self.interp = x
+
+    def setMul(self, x):
+        # set offset
+        self.mul = x
+
+    def setAdd(self, x):
+        # set offset
+        self.add = x
+
+
 ### function to distribute sounds in stereo pairs
 
 def panMove(snd0,snd1,fil,nch,mult):
