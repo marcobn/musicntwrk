9e016357d3419431ca15d401de32baf2439a7841
Marco Buongiorno Nardelli <mbn@unt.edu>
2025-07-09 10:44:33 -0600

    correction to tonalAnalysis + add transMatrix

--- musicntwrk-2.0/src/harmony/tonalAnalysis.py
+++ musicntwrk-2.0/src/harmony/tonalAnalysis.py
@@ -23,8 +23,8 @@ from ..utils.generalizedOpsName import generalizedOpsName
 
 def tonalAnalysis(chords,sections,key,enharm=[['C','C']],write=None):
     chs = []
-    for c in chords.recurse().getElementsByClass('Chord'):
-        chs.append(c)
+    for c in chords:
+        chs.append(m21.chord.Chord(c))
     regionKey = []
     for n in range(len(sections)-1):
         ini = sections[n]
@@ -57,8 +57,9 @@ def tonalAnalysis(chords,sections,key,enharm=[['C','C']],write=None):
     if write:
         # write the analyzed score in musicxml
         l = 0
-        analyzed = copy.deepcopy(chords)
-        for c in analyzed.recurse().getElementsByClass('Chord'):
+        analyzed = copy.deepcopy(chs)
+        rn_score = m21.stream.Stream()
+        for c in analyzed: #.recurse().getElementsByClass('Chord'):
             c.closedPosition(forceOctave=4,inPlace=True)
             c.addLyric('')
             c.addLyric('')
@@ -76,8 +77,47 @@ def tonalAnalysis(chords,sections,key,enharm=[['C','C']],write=None):
                         c.addLyric(regionKey[l]+':'+str(rnum[l]))
             if l < len(chs)-1: 
                 l += 1
+                rn_score.append(c)
             else: 
                 break
-        analyzed.show('musicxml')
+
+        rn_score.show('musicxml')
         
     return(analysis)
+
+def string2Chord(ch,key):
+    s = []
+    i=0
+    while i < len(ch):
+        if i+1 < len(ch):
+            if ch[i+1] == '-':
+                e = ch[i]+'-'
+                i += 2
+            elif ch[i+1] == '#':
+                e = ch[i]+'#'
+                i += 2
+            else:
+                e = ch[i]
+                i += 1
+        else:
+            e = ch[i]
+            i += 1    
+        s.append(e)
+    # get RN
+    rn = m21.roman.romanNumeralFromChord(m21.chord.Chord(s),m21.key.Key(key)).figure
+    return(s,rn)
+
+def transMatrix(Gedges,idx,key='C',N=10):
+    # calculate weights of edges of directional network (transition probability matrix)
+    idsec = idx
+    edge,weight = np.unique(np.array(Gedges[idsec][["Source","Target"]],dtype=int),return_counts=True,axis=0)
+    weight = weight/len(Gedges[idsec])*100
+    tmp = []
+    for i in range(len(edge)):
+        _,rn0 = string2Chord(Gnodes[idsec].iloc[edge[i][0]][0],key)
+        _,rn1 = string2Chord(Gnodes[idsec].iloc[edge[i][1]][0],key)
+        tmp.append([Gnodes[idsec].iloc[edge[i][0]][0],Gnodes[idsec].iloc[edge[i][1]][0],
+                    rn0,rn1,weight[i]])
+    transmatrix = pd.DataFrame(tmp,columns=['Source','Target','RN source','RN target','Weight'])
+    print(transmatrix.sort_values('Weight',ascending=False).head(n=N).to_string(index=False))
+    return(transmatrix.sort_values('Weight',ascending=False))
